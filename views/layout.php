ca3caed9bc22c665b5020b4eeed3faf0af7f5183
Lucas <lukaspereyra8@gmail.com>
2020-10-22 20:01:45 -0300

    changed css librari, first gui completed

---
+++ views/layout.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trazabilidad de Asistencia - Gym Tracking App</title>
+    <link 
+        href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,300;0,400;1,100&display=swap" 
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/purecss@2.0.3/build/base-min.css">
+    <link rel="stylesheet" href="https://unpkg.com/purecss@2.0.3/build/grids-min.css">
+    <link rel="stylesheet" href="https://unpkg.com/purecss@2.0.3/build/forms-min.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/utils.css">
+</head>
+<body>
+    <div class="container bg-black-1">
+        <div class="pure-g">
+            <div class="pure-u-1-1 content-center">
+                <h1 class="main-title text-upper text-green-1 ff-1">Gym tracking App</h1>
+            </div>
+        </div>
+        
+        <div class="pure-g">
+            <div class="pure-u-1-1">
+                <form class="pure-form pure-g register-form">
+                    <div class="pure-u-16-24">
+                        <input type="text" placeholder="Ingrese su DNI o Nombre" class="line-input styled-input m-r-2 ff-1" />
+                    </div>
+                    <div class="pure-u-1-24"></div>
+                    <div class="pure-u-5-24">
+                        <button 
+                            type="submit" 
+                            class="pure-button pure-button-primary line-button styled-button bg-black-1 text-upper text-green-1 ff-1">
+                            Registrar Ingreso
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
